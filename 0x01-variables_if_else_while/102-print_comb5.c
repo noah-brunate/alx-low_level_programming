7eb8164b04dcc82ff48e212f636408996ee44653
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-04 10:21:32 -0700

    c world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include<stdio.h>
 /**
 * main - entry point
 *
@@ -7,25 +7,27 @@
 */
 int main(void)
 {
-int i, j;
-for (j = 0; j <= 99; j++)
-{
-for (i = j; i <= 99; i++)
-{
-if (i != j)
-{
-putchar(j / 10 + 48);
-putchar(j % 10 + 48);
-putchar(' ');
-putchar(i / 10 + 48);
-putchar(i % 10 + 48);
-if (j * 100 + i != 9899)
-{
-putchar(',');
-putchar(' ');                                          h}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+
+	for (j = 0; j <= 99; j++)
+	{
+	for (i = j; i <= 99; i++)
+	{
+	if (i != j)
+	{
+	putchar(j / 10 + 48);
+	putchar(j % 10 + 48);
+	putchar(' ');
+	putchar(i / 10 + 48);
+	putchar(i % 10 + 48);
+	if (j * 100 + i != 9899)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
