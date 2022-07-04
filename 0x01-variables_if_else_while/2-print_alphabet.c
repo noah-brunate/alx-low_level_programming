79a0a13b704c1ded6cc5851301c2f90c73edb613
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-04 08:37:11 -0700

    c world

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 /**
- * main - entry point
- *
- * Return: always return 0
- *
- */
+* main - entry point
+*
+* Return: always return 0
+*
+*/
 int main(void)
 {
-char a;
-for (a = 'a'; a <= '2' ; a++)
-putchar(a);
-putchar('\n);
-return (0);
+	char a;
+
+	for (a = 'a'; a <= '2' ; a++)
+	putchar(a);
+	putchar('\n);
+	return (0);
 }
+
