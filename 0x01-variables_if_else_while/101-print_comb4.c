e79037275746d278e2d3ec885965d8133aa03d11
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-04 10:33:47 -0700

    c world

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include<stdio.h>
 /**
 * main - entry point
 *
@@ -6,30 +6,32 @@
 *
 */
 int main(void)
-{                                                       int i, j, k;
-for (i = 48; i < 58; i++)
 {
-for (j = i; j < 58; j++)
-{
-for (k = j; k < 58; k++)
-{
-if (i == j || j == k || i == k)
-{
-continue;
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+	for (j = i; j < 58; j++)
+	{
+	for (k = j; k < 58; k++)
+	{
+	if (i == j || j == k || i == k)
+	{
+	continue;
+	}
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i == 55 && j == 56 && k == 57)
+	{
+	break;
+	}
+	else
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
-putchar(i);
-putchar(j);
-putchar(k);
-if (i == 55 && j == 56 && k == 57)
-{
-break;
-}
-else
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);                                             }
