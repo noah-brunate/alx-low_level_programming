ad5f69a436a19950cc9f4c3f57bde65c13e685b6
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-04 09:55:54 -0700

    c world

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,11 @@
 */
 int main(void)
 {
-int a = 0;
-for (; a < 10 ; a++)
-putchar('0' + a);
-putchar('\n);
-return (0);
+	int a = 0;
+
+	for (; a < 10 ; a++)
+	putchar('0' + a);
+	putchar('\n');
+	return (0);
 }
+
