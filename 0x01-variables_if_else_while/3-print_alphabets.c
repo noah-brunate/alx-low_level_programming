617d53ae6763462ba1ed21ff3ff24274e1b065b0
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-04 09:26:06 -0700

    c world

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-char a;
-for (a = 'a'; a <= '2' ; a++)
-putchar(a);
-for (a = 'A'; a <= 'Z' ; a++)
-putchar(a);
-putchar('\n);
-return (0);
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	putchar(a);
+	for (a = 'A'; a <= 'Z' ; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
 }
 
