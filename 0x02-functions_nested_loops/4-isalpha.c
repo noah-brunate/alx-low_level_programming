759121981c0b35aa6de69cb436a8c793e8118653
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-06 10:34:05 -0700

    c functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,8 @@
 * @c: a charter to be checke on
 * Return: returns 0 or 1 depending on condition
 */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	return ((c >= 'a'  && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	return ((c >= 'a'  && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
