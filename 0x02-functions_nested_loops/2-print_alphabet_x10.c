91b49c5aeb20dc35023d9b9b4913531e97d9a290
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-06 13:23:51 -0700

    c functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,14 @@
 /**
 * print_alphabet_x10 -> prints the lowercase alphabets
 */
-print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int j;
 	int x;
 
-	for (x = 0; x < 10; x; x++)
+	for (x = 0; x < 10; x++)
 	{
-	for (j = 'a'; j <= 'z' j++)
+	for (j = 'a'; j <= 'z'; j++)
 	{
 	_putchar(j);
 	}
