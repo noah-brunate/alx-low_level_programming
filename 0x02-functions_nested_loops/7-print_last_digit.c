5765fe98c342258436d4dc98f215b613bab5af11
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-06 11:12:31 -0700

    c functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
 * print_last_digit -> prints the last digit
 * @n: the passed args
@@ -11,7 +11,6 @@ int print_last_digit(int)
 	if (n > 0)
 	n = -n;
 	x = n % 10;
-	_putchar(x + '0')
+	_putchar(x + '0');
 	return (x);
 }
-
