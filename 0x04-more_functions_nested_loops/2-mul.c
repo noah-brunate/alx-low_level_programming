8ec75749d360c6fd71eb74f0210fffdee76023cb
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-07 13:12:08 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+* mul -> multiplying 2 number
+* @a: integer param 1
+* @b: integer param 2
+* Return: Success and intiger
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
