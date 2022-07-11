08edadf6f6d953ae4ae17508e39f6145c7376140
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-11 12:36:27 -0700

    c pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,19 @@
 #include "main.h"
 /**
-* void print_array -> prints elements
+* print_array -> prints elements
 * @a: parameter1
 * @n: parameter2
 * Return: elements of an array of integers
 */
 void print_array(int *a, int n)
 {
-	int x[n];
-	char i, j;
+	int [n];
+	char j;
 
-	for (i = 0; i < n; i++)
-	{
-		x[i] = i;
-	}
 	for (j = 0; j < n; j++)
 	{
-		_putchar("Element[%d] = %d, \n", j, x[j]);
+		printf("Element[%d] = %d, \n", j, [j]);
 	}
-	_putchar('\n');
 	return (0);
 }
 
