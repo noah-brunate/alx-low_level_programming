f059c6c72ffa3eddc64a2aafea07e5443f8015d9
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-13 08:29:03 -0700

    c more pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* string_toupper -> converting any string to upper case
+* @X: string param
+* Return: string
+*/
+char *string_toupper(char *X)
+{
+	int i = 0;
+
+	while (X[i])
+	{
+		if (X[i] >= 97 && X[i] <= 122)
+			X[i] = X[i] - 32;
+		i++;
+	}
+	return (X);
+}
+
