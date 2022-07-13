494c2c74cccc88919d38c7c661a0772deeb61780
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-13 11:26:33 -0700

    c more pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
-* reverse_array -> reversing an array
-* @a: array a
-* @n: an element of an array
+* leet -> encodes a string into 1337
+* @s: input string.
+* Return: the pointer to dest.
 */
-void reverse_array(int *a, int n)
-{
-	int *p, i, aux, k;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < n / 2; k++)
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*((s + count) = numbers[i]);
+				break;
+			}
+		}
+		count++;
 	}
+
+	return (s);
 }
 
