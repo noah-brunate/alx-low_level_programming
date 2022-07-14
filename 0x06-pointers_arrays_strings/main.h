eca7909ff1f3f388a8c1809c953839fe056c09cc
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-14 07:48:05 -0700

    c more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
 
