0f2fd09f354691d9f459b227e42dd5ba6ad876a9
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-07-18 14:05:30 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _memset -> this is a memory set function
+* @s: string
+* @b: a character
+* @n: an integer
+* Return: a string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
+
