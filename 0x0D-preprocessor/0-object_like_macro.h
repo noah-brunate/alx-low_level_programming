1ba8965f4194c7e8415531773c924b9b68f74459
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-01 10:31:58 -0700

    preprocessors

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,4 @@
-#include <stdio.h>
-
+#ifndef SIZE
 #define SIZE 1024
+
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,12 +1,2 @@
-#include <stdio.h>
-
 #define SUM(x, y) ((x) + (y))
-
-void main(void)
-{
-	int x, y;
-	result = SUM(x, y)
-
-	return (result);
-}
-
+return SUM(x, y);
