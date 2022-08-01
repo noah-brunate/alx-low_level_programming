55e3d3269cbb04f02760671dee1eae6089dd9c5f
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-01 14:06:11 -0700

    structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+* struct dog -> the new data type
+* @name: atribute pointer to char name's dog
+* @age: atribute float age's dog
+* @owner: atribute pointer to char owner's dog
+*/
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
+
+
