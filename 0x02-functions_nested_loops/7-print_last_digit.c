89726d4a79e18065eb2ef7437cbab639bd9a9a41
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-06 08:07:49 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,12 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	long int last_digit;
 
 	if (n < 0)
-	{
 		n * (-1);
-	}
-		last_digit = (n % 10);
-		_putchar(last_digit);
-		return (last_digit);
+	last_digit = n % 10;
+	_putchar(last_digit);
+	return (last_digit);
 }
 
