eba911b6a4727f007a5a656c89ac654b2a8db2bc
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-07 12:29:11 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@ void print_line(int n)
 {
 	int x;
 
-	if (x <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x <= n; x++)
-	{
+		for (x = 0; x <= n; x++)
+		{
 		_putchar('_');
-	}
+		}
 	_putchar('\n');
 	}
 }
