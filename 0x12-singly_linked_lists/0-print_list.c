150992461ec09274089b54f7a95d57f968ea7e89
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-11 13:39:27 -0700

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ size_t print_list(const list_t *h)
 	size_t nelem;
 
 	nelem = 0;
-	while (h != '\0')
+	while (h != NULL)
 	{
-		if (h->str == '\0')
+		if (h->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
@@ -21,8 +21,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-	h = h->next;
-	nelem++;
+		h = h->next;
+		nelem++;
 	}
 	return (nelem);
 }
