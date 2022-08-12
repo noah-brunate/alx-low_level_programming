92a0667c0d5bcf6c7ece984a950da8f7f53143d4
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-12 11:19:00 -0700

    singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,9 +3,26 @@
 /**
 * add_node - function adds a new node at the beginning
 * @head: pointer to apointer
-*
+* @str: node string
 * Return: returns the address of the new element or NULL
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	unsigned int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = NULL;
+
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
+
 
