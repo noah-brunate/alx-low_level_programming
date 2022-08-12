ead45d80e344a8096b4d0412d272fd907138f996
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-12 12:12:44 -0700

    singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,27 @@
+#include "lists.h"
+
+/**
+* add_node_end - function adds node at end
+* @head: head pointer
+* @str: string pointer
+*
+* Return: return pointer to the new element or NULL
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *ptr;
+	unsigned int len;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	ptr = *head;
+	while (ptr != NULL)
+		new->len = len;
+		ptr = ptr->next;
+	ptr->next = new;
+	return (new);
+}
 
