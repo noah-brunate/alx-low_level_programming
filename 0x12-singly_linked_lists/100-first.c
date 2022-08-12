91fee870b9957ce4f923bdaf04b4ed7567e19a73
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-12 13:31:28 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* __attribute__ - executes function before main
+* print - function prints before main is executed
+* @constructor: function param
+*
+*/
+
+void __attribute__((constructor) print(void)
+
+/**
+* print - function prints before main is executed
+*/
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n")
+	printf("I bore my house upon my back!\n");
+}
+
+
+
