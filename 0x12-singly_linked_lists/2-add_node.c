83d2682cf374ef8b9e3ed1361127e6553222138e
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-12 11:39:13 -0700

    singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,20 +9,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int i;
+	unsigned int len = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = i;
-	new->next = NULL;
-
+	while (str[len])
+		len++;
+	new->len = len;
 	new->next = *head;
 	*head = new;
 	return (*head);
 }
-
 
 
