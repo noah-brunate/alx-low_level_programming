2abce5a4b8dd4c8cfb97303b7c6f115890976caf
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-12 08:51:59 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* list_len - function returns number of elements
+* @h: pointer to nodes
+*
+* Return: returns number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	int nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next
+		nelem++;
+	}
+	return (nelem);
+}
+
+
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* add_node - function adds a new node at the beginning
+* @head: pointer to apointer
+*
+* Return: returns the address of the new element or NULL
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+
