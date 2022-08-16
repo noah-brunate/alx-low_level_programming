df300a891056861b70d7989c0a6e072bd8e63494
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-16 12:08:53 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,11 +6,14 @@
 */
 void free_listint(listint_t *head)
 {
+	listint_t *tmp;
+
 	while (head != NULL)
 	{
+		tmp = head;
 		head = head->next;
-		free(head->next);
+		free(tmp);
 	}
-	free(head);
 }
+
 
