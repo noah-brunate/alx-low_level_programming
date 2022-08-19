dd595016adead4d98cc141ff1b082394efcdf6ad
NIWAMANYA BRUNO <noahbrunate9@gmail.com>
2022-08-19 08:32:50 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0;
 	unsigned int j = 1;
 
+	if (*(b + i) == '\0' || *(b + i) != ('0' || '1'))
+	{
+		return (0);
+	}
+
 	while (*(b + i) != '\0')
 	{
 		if (*(b + i) == '0')
