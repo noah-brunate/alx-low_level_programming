e5029761e216ea63c78f6d48e4040cae3d4151f1
noah-brunate <noahbrunate9@gmail.com>
2023-03-26 02:48:18 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* atoi - function converts string to integer
+* @s: string to convert
+*
+* Return: integer else 0 if no conversion
+*/
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
+
