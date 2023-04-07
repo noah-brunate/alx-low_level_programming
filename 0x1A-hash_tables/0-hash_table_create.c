b8f5802859dfae7b930b9443b8778e5db4d7911a
noah-brunate <noahbrunate9@gmail.com>
2023-04-07 07:23:35 -0700

    implementing hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - function creates a hash table
+* @size: size of the array
+*
+* Return: pointer to the newly created hash table else NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *));
+
+	if (table->array)
+		return (NULL);
+	for (int i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
+
+
+
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This directory is about implementation of hash tables
