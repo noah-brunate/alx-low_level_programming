b67f3125a09a3ffd4edb6b93c960ce2eb7cc249a
noah-brunate <noahbrunate9@gmail.com>
2023-04-29 07:24:50 -0700

    malloc and free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,15 @@ int **alloc_grid(int width, int height)
 	{
 		p[i] = malloc(width * sizeof(*p));
 		if (p[i] == NULL)
+		{
+			for (i--; i =< 0; i--)
+			{
+				free(p[i]);
+			}
+			free(p);
+
 			return (NULL);
+		}
 
 		for (j = 0; j < width; j++)
 		{
